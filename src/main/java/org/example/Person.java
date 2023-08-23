94a3df4433c7a07d8feb4ab1537f5da18481318e
Machin_Gon2 <luisyamilgarcia@gmail.com>
2023-08-22 19:53:47 -0600

    Task 1

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.example.Job;
+
+public class Person {
+    private String name;
+    private Job myJob;
+
+    public Person(String name, Job myJob) {
+        this.name = name;
+        this.myJob = myJob;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Job getMyJob() {
+        return myJob;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMyJob(Job myJob) {
+        this.myJob = myJob;
+    }
+}
