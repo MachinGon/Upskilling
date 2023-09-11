e57bfcf10a48155ab72d9555eed1335987655bf2
Machin_Gon2 <luisyamilgarcia@gmail.com>
2023-09-10 20:43:53 -0600

    Task 1

--- src/main/java/org/collection/Main2.java
+++ src/main/java/org/collection/Main2.java
@@ -3,6 +3,7 @@ package org.collection;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main2 {
@@ -14,5 +15,10 @@ public class Main2 {
                 .map(String :: toLowerCase) //all lowerCase to avoid UpperCase ocurrences
                 .collect(Collectors.groupingBy(s->s, LinkedHashMap::new, Collectors.counting())); //lambda function for group in Hashmap
         System.out.println(map);
+
+        for(Map.Entry entry : map.entrySet()){
+            System.out.println(entry.getKey() +" : "+entry.getValue());
+        }
+
     }
 }
