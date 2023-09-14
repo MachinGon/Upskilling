be51519fae1637cd4905b19e4c937b254afdf8bc
Machin_Gon2 <luisyamilgarcia@gmail.com>
2023-09-13 23:24:11 -0600

    Task 1

--- src/main/java/org/collection/Main2.java
+++ src/main/java/org/collection/Main2.java
@@ -1,10 +1,8 @@
 package org.collection;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.Map.Entry;
 
 public class Main2 {
     public static void main(String[] args){
@@ -12,13 +10,73 @@ public class Main2 {
                 "labore et dolore magna aliqua";
         HashMap<String,Long> map;
         map = Arrays.stream(loremIpsum.split("")) //separates String by char, if "" then all of them
-                .map(String :: toLowerCase) //all lowerCase to avoid UpperCase ocurrences
-                .collect(Collectors.groupingBy(s->s, LinkedHashMap::new, Collectors.counting())); //lambda function for group in Hashmap
-        System.out.println(map);
+                //.map(String :: toLowerCase) //all lowerCase to avoid UpperCase ocurrences
+                .collect(Collectors.groupingBy(String::toLowerCase, LinkedHashMap::new, Collectors.counting())); //lambda function for group in Hashmap
+        System.out.println("Unsorted HashMap: "+map);
 
-        for(Map.Entry entry : map.entrySet()){
-            System.out.println(entry.getKey() +" : "+entry.getValue());
+        HashMap<String,Long> sortedMap = sortMinToMaxMap(map);
+        System.out.println("Sorted HashMap: "+sortedMap);
+        System.out.println("char menos repetido: "+sortedMap.entrySet().stream().findFirst().get().getKey());
+
+        HashMap<String,Long> reverseSortedMap = sortMaxToMinMap(map);
+        System.out.println("char mas repetido: "+reverseSortedMap.entrySet().stream().findFirst().get().getKey());
+
+
+        TreeMap<String,Long> tree;
+        tree = Arrays.stream(loremIpsum.split(""))
+                .collect(Collectors.groupingBy(String::toLowerCase, TreeMap::new, Collectors.counting()));
+        System.out.println("Alphabetical order Treemap: "+tree);
+        tree = new TreeMap<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int compare = map.get(o1).compareTo(map.get(o2));
+                if (compare==0)
+                    return 1;
+                else
+                    return compare;
+            }
+        });
+        tree.putAll(map);
+        System.out.println("Value order Treemap: "+tree);
+        System.out.println("Treemap Min:"+ tree.pollFirstEntry().getKey() + "\nTreemap Max: "+tree.pollLastEntry().getKey());
+
+
+
+        //functional interface -> (lista -> s, ... )
+        
+//        for(Map.Entry entry : map.entrySet()){
+//            System.out.println(entry.getKey() +" : "+entry.getValue());
+//        }
+
+    }
+
+    public static HashMap<String,Long> sortMaxToMinMap(Map<String, Long> map){
+        List<Map.Entry<String,Long>> entries = new LinkedList<>(map.entrySet());
+        entries.sort(new Comparator<Entry<String, Long>>() {
+            @Override
+            public int compare(Entry<String, Long> o1, Entry<String, Long> o2) {
+                return -o1.getValue().compareTo(o2.getValue());
+            }
+        });
+        HashMap<String,Long> sortedMap = new LinkedHashMap<>();
+        for(Entry<String,Long> entry : entries){
+            sortedMap.put(entry.getKey(),entry.getValue());
         }
+        return sortedMap;
+    }
 
+    public static HashMap<String,Long> sortMinToMaxMap(Map<String, Long> map){
+        List<Map.Entry<String,Long>> entries = new LinkedList<>(map.entrySet());
+        entries.sort(new Comparator<Entry<String, Long>>() {
+            @Override
+            public int compare(Entry<String, Long> o1, Entry<String, Long> o2) {
+                return o1.getValue().compareTo(o2.getValue());
+            }
+        });
+        HashMap<String,Long> sortedMap = new LinkedHashMap<>();
+        for(Entry<String,Long> entry : entries){
+            sortedMap.put(entry.getKey(),entry.getValue());
+        }
+        return sortedMap;
     }
 }
